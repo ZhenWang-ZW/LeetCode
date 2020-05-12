aba45b7f5d0727cfc762b43cf5e0a6c78db99dca
Zhen Wang <zhen446@hotmail.com>
2020-05-12 23:02:17 +0100

    week2 + DP

--- src/LeetCode/May/Problems_Set_1.java
+++ src/LeetCode/May/Problems_Set_1.java
@@ -1,6 +1,8 @@
 package LeetCode.May;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Problems_Set_1 {
 
@@ -16,6 +18,23 @@ public class Problems_Set_1 {
 		
 		int[][] map= new int[][] {{0,0},{1,0}};
 		System.out.println(uniquePathsWithObstacles(map));
+		
+		int[][]grid=new int[][] {{1,2,5},{3,2,1}};
+		System.out.println(minPathSum(grid));
+		
+		System.out.println(numDecodings("12"));
+		
+		System.out.println(numTrees(5));
+		
+		List<List<Integer>> triangle = new ArrayList() {
+			{
+				add(new ArrayList() {{add(-1);}});
+				add(new ArrayList() {{add(2); add(3);}});
+				add(new ArrayList() {{add(1); add(-1); add(-3);}});
+			}
+		};
+		System.out.println(minimumTotal(triangle));
+		
 		
 	}
 	
@@ -192,5 +211,107 @@ public class Problems_Set_1 {
         }
         return nums[n];
     }
+	
+	/* 64. Minimum Path Sum*/
+	public static int minPathSum(int[][] grid) {
+		int m=grid.length,
+			n=grid[0].length;
+		
+		int[][] p=new int[m][n];
+		p[0][0]=grid[0][0];
+		
+		for(int r=1; r<m;r++) {
+			p[r][0]=p[r-1][0]+grid[r][0];
+		}
+		for(int c=1; c<n;c++) {
+			p[0][c]=p[0][c-1]+grid[0][c];
+		}
+		
+		for(int i=1; i<m; i++) {
+			for(int j=1; j<n; j++) {
+				p[i][j]=Math.min(p[i-1][j]+grid[i][j], p[i][j-1]+grid[i][j]);
+			}
+		}
+		return p[m-1][n-1];
+    }
+	
+	/* 91. Decode Ways*/
+	public static int numDecodings(String s) {
+        int len=s.length();
+        
+        int[] nums = new int[len+1];
+        if(s.startsWith("0") || s.contains("00")) return 0;
+        if(len==0) return 1;
+        if(len==1) return 1;
+        nums[0] = 1;
+        nums[1] = (s.charAt(0) == '0') ? 0 : 1;
+        for(int i=2; i<=len;i++) {
+        	int curDig = s.charAt(i-1)-'0';
+        	int twoDigs= (s.charAt(i-2)-'0')*10+curDig;
+        	if(curDig>0) {
+        		nums[i]=nums[i-1];
+        	}
+        	if(twoDigs>=10 && twoDigs<=26) {
+        		nums[i]+=nums[i-2];
+        	}
+        }
+        
+        return nums[len];
+    }
+	
+	/* 96. Unique Binary Search Trees*/
+	public static int numTrees(int n) {
+		int[] dp = new int[n+1];
+	    //initialize
+	    dp[0]=1;// dp[0]initialize as 1,null tree [] is a tree
+	    
+	    for(int i =1;i<=n; i++)// Number of binary search numbers from 1 ... n
+	        for(int j = 1; j<=i;j++)// Gradually choose 1 ... n as the root node
+	            dp[i] += dp[j-1]*dp[i-j];// j-1 number on the left, i-j number on the right
+	                      
+	    return dp[n]; 
+    }
+	
+	/* 120. Triangle*/
+	public static int minimumTotal(List<List<Integer>> triangle) {
+		List<List<Integer>> tSum = new ArrayList();
+		List<Integer> sub = new ArrayList<Integer>() {
+			{add(triangle.get(0).get(0));}
+		};
+		tSum.add(sub);
+		
+		for(int i=1; i<triangle.size(); i++) {
+			sub=new ArrayList();
+			for(int j=0; j<triangle.get(i).size(); j++) {	
+				if(j>0 && j<triangle.get(i).size()-1) {
+					int preSum_1 = tSum.get(i-1).get(j-1);
+					int preSum_2 = tSum.get(i-1).get(j);
+					sub.add(Math.min(triangle.get(i).get(j)+preSum_1, triangle.get(i).get(j)+preSum_2));
+				}else if(j==triangle.get(i).size()-1) {
+					int preSum = tSum.get(i-1).get(j-1);
+					sub.add(triangle.get(i).get(j)+preSum);
+				}
+				else {
+					int preSum = tSum.get(i-1).get(j);
+					sub.add(triangle.get(i).get(j)+preSum);
+				}
+			}
+			tSum.add(sub);
+		}
+		int res=Integer.MAX_VALUE;
+		for(Integer a: tSum.get(tSum.size()-1)) {
+			if(a<res) {
+				res=a;
+			}
+		}
+		return res;
+    }
+	
+	/* 139. Word Break*/
+	public static boolean wordBreak(String s, List<String> wordDict) {
+        
+		return false;
+    }
+	
 
 }
